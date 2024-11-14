e2c3fe5de12a190a0bacf596c933e407c3fd19df
Satyam-Nayak <satyamn120@gmail.com>
2024-11-14 19:43:26 +0530

    create update delete read

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Compo/Home'
+import Create from './Compo/Create'
+import Update from './Compo/Update'
+import Read from './Compo/Read'
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/create' element={<Create/>}></Route>
+      <Route path='/update/:id' element={<Update/>}></Route>
+      <Route path='/read/:id' element={<Read/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
